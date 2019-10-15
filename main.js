218b07238e24ced91f3bf039e566c6f6c4d3848a
Adam Kovacs <adam.kovacs@Nology.local>
2019-10-15 19:44:33 +0100

    setup

---
+++ main.js
@@ -0,0 +1,25 @@
+
+let keyA = document.getElementById("a-key");
+
+const keyPressed = () => {
+  keyA.classList.add("pressed");
+}
+
+const keyLift = () => {
+  keyA.classList.remove("pressed");
+}
+
+
+document.onkeydown = pressEvent => {
+  if(pressEvent.key === "a"){
+   keyPressed();
+  }
+}
+
+
+document.onkeyup = liftEvent =>{
+  if(liftEvent.key === "a"){
+  keyLift();
+  } 
+}
+
